994ca1c2e899ef635694cc04d0ddda2335f5180c
Tej Kute <tejkute61@gmail.com>
2025-12-15 06:48:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9332651577-f2a5a",
+  "appId": "1:522213730618:web:0018d717882ea809338803",
+  "apiKey": "AIzaSyAB0lHut9cz3oUTPZ2nb7WUBu4E-38QKlQ",
+  "authDomain": "studio-9332651577-f2a5a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "522213730618"
+};
