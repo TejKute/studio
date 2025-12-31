21ae7709126b8d85988fc2006fb5b8adf8d80de3
Tej Kute <tejkute61@gmail.com>
2025-12-31 03:40:00 +0000

    now highlight it when my cursor goes on the name

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -119,6 +119,7 @@ const CommandItem = React.forwardRef<
     ref={ref}
     className={cn(
       "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50",
+      "hover:bg-accent hover:text-accent-foreground",
       className
     )}
     {...props}
